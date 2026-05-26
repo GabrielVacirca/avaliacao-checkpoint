8aad590059540fa4e9aac0f236b278a15869a63e
Gabriell David <vacircagabriel21@gmail.com>
2026-05-26 00:00:35 -0300

    refactor: atualiza entrada do app

--- usedev_uninassau/lib/main.dart
+++ usedev_uninassau/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:usedev_uninassau/src/screens/initial_screen.dart';
+import 'package:usedev_uninassau/src/services/cart_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +8,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'UseDev',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: InitialScreen(),
+    return CartScope(
+      notifier: CartService.instance,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'UseDev',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const InitialScreen(),
+      ),
     );
   }
 }
