ffbfb18b8efa5226d4b8097b65e6cdb2ac5214df
Gabriell David <vacircagabriel21@gmail.com>
2026-05-26 00:00:46 -0300

    refactor: atualiza card de produto

--- usedev_uninassau/lib/src/widgets/product_card_widget.dart
+++ usedev_uninassau/lib/src/widgets/product_card_widget.dart
@@ -1,50 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:usedev_uninassau/src/models/product_model.dart';
+import 'package:usedev_uninassau/src/screens/product_detail_screen.dart';
 
 class ProductCardWidget extends StatelessWidget {
   const ProductCardWidget({
-    required this.nome,
-    required this.url,
-    required this.preco,
+    required this.product,
     super.key,
   });
 
-  final String nome;
-  final String url;
-  final String preco;
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: .all(20),
+      margin: const EdgeInsets.all(20),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
       elevation: 5,
-      child: Column(
-        crossAxisAlignment: .stretch,
-        children: [
-          Image.network(url, height: 200, width: double.infinity, fit: .cover),
-          Padding(
-            padding: .symmetric(horizontal: 15, vertical: 10),
-            child: Text(
-              nome,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: .bold,
-                fontFamily: GoogleFonts.orbitron().fontFamily,
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (context) => ProductDetailScreen(product: product),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              product.image,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Text(
+                product.title,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: GoogleFonts.orbitron().fontFamily,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: .symmetric(horizontal: 15, vertical: 10),
-            child: Text(
-              preco,
-              style: TextStyle(
-                fontSize: 31,
-                fontFamily: GoogleFonts.poppins().fontFamily,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Text(
+                product.price.toStringAsFixed(2),
+                style: TextStyle(
+                  fontSize: 31,
+                  fontFamily: GoogleFonts.poppins().fontFamily,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
